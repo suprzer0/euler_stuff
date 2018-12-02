d44a8e621a6e014dd9c2fa7cd1d72f559b8ce8e5
Evan Kelsey <ekelsey@mintel.com>
2018-12-02 10:16:51 -0600

    Refactor tests to nicer pytest format.

--- tests.py
+++ tests.py
@@ -1,64 +1,56 @@
-from doctest import DocTestSuite
-import unittest
 from itertools import count
-
-import euler_tools
 from euler_tools import CachedIter, AscendingCachedIter
 
-dtests = DocTestSuite(euler_tools)
+import pytest
 
-class CachedIterTests(unittest.TestCase):
+def test_cachediter_infinite():
+    evens = CachedIter(count(2, 2))
 
-    def test_infinite(self):
-        evens = CachedIter(count(2, 2))
-
-        self.assertEqual(evens[1], 4)
-        self.assertEqual(evens[:2], [2, 4])
-        self.assertEqual(evens[2:5], [6, 8, 10])
-        self.assertEqual(evens[5:15:2], [12, 16, 20, 24, 28])
-        
-        evens_sliced = evens[5:]
-        self.assertIsInstance(evens_sliced, CachedIter)
-        self.assertEqual(evens_sliced[:2], [12, 14])
-
-    def test_multiple_iter(self):
-        """
-        Multiple iterators on the same CachedIter don't affect each other
-        """
-        evens = CachedIter(count(2, 2))
-
-        i1 = iter(evens)
-        i2 = iter(evens)
-
-        self.assertEqual(next(i1), 2)
-        self.assertEqual(next(i2), 2)
-        self.assertEqual(next(i1), 4)
-        self.assertEqual(next(i2), 4)
+    assert evens[1] == 4
+    assert evens[:2] == [2, 4]
+    assert evens[2:5] == [6, 8, 10]
+    assert evens[5:15:2] == [12, 16, 20, 24, 28]
+    
+    evens_sliced = evens[5:]
+    assert isinstance(evens_sliced, CachedIter)
+    assert evens_sliced[:2] == [12, 14]
 
 
-class AscendingCachedIterTests(unittest.TestCase):
-        
-    def test_finite(self):
-        finite = AscendingCachedIter(range(1, 10))
+def test_cachediter_multiple_iter():
+    """
+    Multiple iterators on the same CachedIter don't affect each other
+    """
+    evens = CachedIter(count(2, 2))
 
-        self.assertIn(2, finite)
-        self.assertNotIn(-3, finite)
-        self.assertNotIn(20, finite)
+    i1 = iter(evens)
+    i2 = iter(evens)
 
-    def test_infinte(self):
-        evens = AscendingCachedIter(count(2, 2))
+    assert next(i1) == 2
+    assert next(i2) == 2
+    assert next(i1) == 4
+    assert next(i2) == 4
 
-        self.assertIn(2, evens)
-        self.assertNotIn(3, evens)
 
-    def test_index(self):
-        evens = AscendingCachedIter(count(2, 2))
+def test_ascendingcachediter_finite():
+    finite = AscendingCachedIter(range(1, 10))
 
-        self.assertEqual(evens[4], 10)
-        self.assertEqual(evens.index(10), 4)
+    assert 2 in finite
+    assert -3 not in finite
+    assert 20 not in finite
 
-        with self.assertRaises(ValueError):
-            evens.index(3)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_ascendingcachediter_infinte():
+    evens = AscendingCachedIter(count(2, 2))
+
+    assert 2 in evens
+    assert 3 not in evens
+
+
+def test_ascendingcachediter_index():
+    evens = AscendingCachedIter(count(2, 2))
+
+    assert evens[4] == 10
+    assert evens.index(10) == 4
+    
+    with pytest.raises(ValueError):
+        evens.index(3)
